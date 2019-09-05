a6eddd800bcb8d65394059c54be872f6958ecfa2
ahmettuztasi <tuztasiahmet@gmail.com>
2019-09-05 09:05:05 -0700

    Initial Commit

---
+++ TwitterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterApp
+//
+//  Created by Ahmet Tuztașı on 05/09/2019.
+//  Copyright © 2019 Ahmet Tuztașı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
